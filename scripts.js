c03b41d7a42a70580ca53342493dfbcbc1bd372d
Pranav Harwadekar <115176961+pharwadekar@users.noreply.github.com>
2024-07-27 17:32:11 -0500

    Initial Commit Landing Page

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('button').addEventListener('click', () => {
+    alert('Redirecting to the Chrome Web Store...');
+    // Redirect to the Chrome Web Store link
+    window.location.href = 'https://chrome.google.com/webstore/detail/stillneedtoaddthis';
+});
